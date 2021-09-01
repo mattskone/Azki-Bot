a9ef0dd84c5d602342cdbf012bf563fe66f593d7
cskone <cskone@my.hpu.edu>
2021-09-01 16:12:19 -0700

    Now ignore commas after name

--- Main.py
+++ Main.py
@@ -12,13 +12,14 @@ reddit = praw.Reddit(
 subreddit = reddit.subreddit('Hololive')
 
 
-trigger = "play" # The substring the bot is looking for
+trigger = "play" # If the comment does not have this word, ignore it
 
 for comment in subreddit.stream.comments(): #Grabs every new comment made in the subreddit
     comment_lower = comment.body.lower()
+    comment_mod = comment_lower.replace(",", "")    # Change var name later
     if f" {trigger} " in comment_lower: # Looks for trigger
         # Check if the word before the trigger is a whitelisted VTuber
         with open('Lists\\VTubers.txt') as vtuber_list:
-            comment_split = comment_lower.split()
+            comment_split = comment_mod.split()
             if comment_split[comment_split.index(trigger)-1] in vtuber_list.read():
-                print(comment_lower)
+                print(comment_mod)
