f2bc9f7ea1fc5ed3da2cf7d5666e1aaa46cd1ecc
cskone <cskone@my.hpu.edu>
2021-09-02 12:16:11 -0700

    Name must now exactly match a name on the whitelist. API keys now environment variables

--- Main.py
+++ Main.py
@@ -1,25 +1,19 @@
 import os
 import praw
-from dotenv import load_dotenv
-
-load_dotenv('Credentials.env')
 
 reddit = praw.Reddit(
-    client_id = os.environ.get('CLIENT_ID'),
-    client_secret = os.environ.get('CLIENT_SECRET'),
-    user_agent = os.environ.get('USER_AGENT'))
-    
+    client_id = os.environ.get('AZKI_ID'),
+    client_secret = os.environ.get('AZKI_SECRET'),
+    user_agent = os.environ.get('AZKI_USER_AGENT')) 
 subreddit = reddit.subreddit('Hololive')
 
 
-trigger = "play" # If the comment does not have this word, ignore it
-
-for comment in subreddit.stream.comments(): #Grabs every new comment made in the subreddit
-    comment_lower = comment.body.lower()
-    comment_mod = comment_lower.replace(",", "")    # Change var name later
-    if f" {trigger} " in comment_lower: # Looks for trigger
+for raw_comment in subreddit.stream.comments(): #Grabs every new comment made in the subreddit
+    trigger = "play" # If raw_comment does not have this word, ignore it
+    comment = raw_comment.body.lower().replace(",", "")
+    if f" {trigger} " in comment: # Looks for trigger
         # Check if the word before the trigger is a whitelisted VTuber
         with open('Lists\\VTubers.txt') as vtuber_list:
-            comment_split = comment_mod.split()
-            if comment_split[comment_split.index(trigger)-1] in vtuber_list.read():
-                print(comment_mod)
+            comment_split = comment.split()
+            if comment_split[comment_split.index(trigger)-1] in vtuber_list.read().split("\n"):
+                print(comment)
